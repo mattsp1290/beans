654c7b558cce26c66152d0fe34adf3ecd718c0ba
Matt Spurlin <mattsp1290@gmail.com>
2026-06-13 16:04:53 -0400

    ralph: iteration 11 - review suggestion

--- deps.go
+++ deps.go
@@ -3,6 +3,8 @@
 package beans
 
 import (
+	// Pin upcoming GORM migration runtime and integration-test dependencies until
+	// implementation packages import them directly.
 	_ "github.com/glebarez/sqlite"
 	_ "github.com/testcontainers/testcontainers-go/modules/mysql"
 	_ "gorm.io/datatypes"
